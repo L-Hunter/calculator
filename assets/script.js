c33ab515d399b983a79c12c5f10cf9401db15f4f
TheButlerDidIt <labutler@gmail.com>
2017-05-24 14:57:34 -0400

    sets up calculator

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,32 @@
-// hello js
+var answer;
+
+function add(a,b){
+	answer = a + b;
+}
+
+function multiply(a,b){
+	answer = a * b;	
+}
+
+function divide(a,b){
+	answer = a / b;
+}
+
+function subtract(a,b){
+	answer = a - b;
+}
+
+var equation = "";
+
+function display(x){
+	var display = document.getElementById("display");
+	equation += x;
+	display.textContent = equation;
+	console.log(x);
+}
+
+function refresh(){
+	location.reload();
+}
+
+
