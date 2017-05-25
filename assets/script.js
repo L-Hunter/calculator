ce815e8148b8e5677b78bc39a39d2fe9099be25d
TheButlerDidIt <labutler@gmail.com>
2017-05-25 13:16:37 -0400

    functional calculator

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,18 @@
+
+var calcDisplay = document.getElementById("display");
+var operator = document.getElementsByClassName("operator");
+for (var i = 0; i < operator.length; i++) {
+    operator[i].addEventListener('click', setA);
+}
+
+var equation = "";
+var a;
+var b;
+var op;
+var n = "";
 var answer;
 
-function add(a,b){
+function add(){
 	answer = a + b;
 }
 
@@ -16,13 +28,43 @@ function subtract(a,b){
 	answer = a - b;
 }
 
-var equation = "";
-
 function display(x){
-	var display = document.getElementById("display");
-	equation += x;
-	display.textContent = equation;
-	console.log(x);
+	if (b){
+		alert("Please clear for more calculations.")
+	} else {
+		equation += x;
+		calcDisplay.textContent = equation;
+		if (!(isNaN(x))){
+			n += x;
+		}
+		else {
+			op = x;
+		}
+	}
+}
+
+function setA(){
+	a = parseInt(n);
+	n = "";
+}
+
+function setB(){
+	b = parseInt(n);
+	n = "";
+}
+
+function calculate(){
+	setB();
+	if (op === "+"){
+		add(a,b);
+	} else if (op === "-"){
+		subtract(a,b);
+	} else if (op === "*"){
+		multiply(a,b);
+	} else if (op === "/"){
+		divide(a,b);
+	}
+	calcDisplay.textContent = answer;
 }
 
 function refresh(){
